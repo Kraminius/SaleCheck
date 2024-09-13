fd712440f22eaa237b782efe29615316180911c9
Mikkel Jürs <s224279@dtu.dk>
2024-09-13 15:38:35 +0200

    Added endpoint to test connection on server.

---
+++ Controllers/AnalyseController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaleCheck.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnalyseController : ControllerBase
+    {
+        [HttpPost("HTML")]
+        public ActionResult<int> DetectSaleFromHTML([FromBody] string htmlFile)
+        {
+            if (string.IsNullOrEmpty(htmlFile))
+            {
+                // Return a 400 Bad Request if the input is null or empty
+                return BadRequest("Invalid HTML content.");
+            }
+
+            int salePercentage = DetectSalePercentage(htmlFile);
+
+            return Ok(salePercentage);
+        }
+
+        //The basics of basic logic. Replace with proper logic. Just here to open the endpoint to test that.
+        private int DetectSalePercentage(string htmlContent)
+        {
+            if (htmlContent.Contains("50% off", StringComparison.OrdinalIgnoreCase))
+            {
+                return 50;
+            }
+            
+            else if (htmlContent.Contains("Sale", StringComparison.OrdinalIgnoreCase))
+            {
+                return 10;
+            }
+            return 0;
+        }
+    }
+}
