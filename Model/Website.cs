24d40fca4fe01530286cf091fdc085e5c5101e5c
Mikkel Jürs <s224279@dtu.dk>
2024-09-17 13:39:19 +0200

    Added Dictionary to store sitemap hashes with the sitemap url to see if the sitemap changed or not.

--- Model/Website.cs
+++ Model/Website.cs
@@ -5,6 +5,10 @@
         public required string WebsiteId { get; set; }
         public required string WebsiteName { get; set; }
         public required string WebsiteUrl { get; set; }
+
+        // Dictionary to store sitemap URL and its hashed value (to see if it changed since last visit)
+        public required Dictionary<string, string> Sitemap { get; set; } = new Dictionary<string, string>();
+
         public string? WebsiteType { get; set; }
         public List<Product>? Products { get; set; }
     }
