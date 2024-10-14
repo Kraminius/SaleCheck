97e7b147ca3635e11f719faa45e6d6753a82c84d
Mikkel Jürs <mikjurs@gmail.com>
2024-10-15 01:08:05 +0200

    Added Scheduler to run our datafactory model on vinduesgrossisten (just this one for now), adding more later.

---
+++ Model/Utility/ScheduledTaskService.cs
@@ -0,0 +1,52 @@
+﻿using Cronos;
+using SaleCheck.Model.Utility;
+
+public class ScheduledTaskService : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly CronExpression _cronExpression;
+    private readonly TimeZoneInfo _timeZoneInfo;
+
+    public ScheduledTaskService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+        _cronExpression = CronExpression.Parse("0 4 * * *");
+        _timeZoneInfo = TimeZoneInfo.Local; 
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var nextRun = _cronExpression.GetNextOccurrence(DateTimeOffset.Now, _timeZoneInfo);
+            if (nextRun.HasValue)
+            {
+                var delay = nextRun.Value - DateTimeOffset.Now;
+                if (delay > TimeSpan.Zero)
+                {
+                    await Task.Delay(delay, stoppingToken);
+                }
+
+                await RunScheduledTask(stoppingToken);
+            }
+        }
+    }
+
+    private async Task RunScheduledTask(CancellationToken cancellationToken)
+    {
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var dataFactory = scope.ServiceProvider.GetRequiredService<DataFactory>();
+            try
+            {
+                Console.WriteLine("Running scheduled task: Scrape vinduesgrossisten");
+                await dataFactory.UpdateWebsiteByCheckingExistingSubsites("vinduesgrossisten", true);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Already updated today, skipping...");
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
