5372eeff3c8d04d56212f9fc0bedbfede285288a
Mikkel Jürs <mikjurs@gmail.com>
2024-10-15 01:09:52 +0200

    Added modellayer WebsiteInit.cs, to serve an endpoint where we want to fill in the rest of the data from the scraper (like sitemaps, products etc).

---
+++ Model/DataClasses/WebsiteInit.cs
@@ -0,0 +1,8 @@
+﻿namespace SaleCheck.Model.DataClasses;
+
+public class WebsiteInit
+{
+    public required string websiteName { get; set; }
+    public required string websiteUrl { get; set; }
+    public required string websiteId { get; set; }
+}
