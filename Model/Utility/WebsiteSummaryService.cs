1abc061042f8f0fbd5e7e56d81b51a97b6b845ee
Mikkel Jürs <mikjurs@gmail.com>
2024-10-15 01:11:04 +0200

    Added service layer to serve data for endpoint.

---
+++ Model/Utility/WebsiteSummaryService.cs
@@ -0,0 +1,43 @@
+﻿using SaleCheck.Model.DataClasses;
+using SaleCheck.Tests.SaleCheck.Tests.Model.Utility;
+
+namespace SaleCheck.Model.Utility;
+
+public class WebsiteSummaryService
+{
+    private readonly ProductAnalysisService _productAnalysisService;
+
+    public WebsiteSummaryService(ProductAnalysisService productAnalysisService)
+    {
+        _productAnalysisService = productAnalysisService;
+    }
+
+    public WebsiteSummary CreateWebsiteSummary(Website website)
+    {
+        var productsOnSale = website.Products.Count(p => p.Price.Any(price => price.DiscountPrice > 0));
+
+        // Get the earliest entry date across all products to check how long we've scraped the site
+        var earliestDate = _productAnalysisService.GetEarliestEntryDate(website.Products);
+
+        // Determine if we have scraped long enough to check for violations
+        var canCheckViolations = _productAnalysisService.CanCheckForViolations(earliestDate);
+
+        // Count the number of products violating the marketing law
+        var productsViolation = canCheckViolations
+            ? website.Products.Count(p => _productAnalysisService.IsProductInViolation(p.Price))
+            : 0;
+
+        var longestStreak = website.Products.Max(p => p.Price.Max(price => (DateTime.UtcNow - price.Date).TotalDays));
+
+        return new WebsiteSummary
+        {
+            WebsiteId = website.WebsiteId,
+            WebsiteUrl = website.WebsiteUrl,
+            WebsiteName = website.WebsiteName,
+            ProductsCount = website.Products.Count,
+            ProductsOnSale = productsOnSale,
+            ProductsViolation = productsViolation,
+            ProductsLongestCurrentStreak = (int)longestStreak
+        };
+    }
+}
