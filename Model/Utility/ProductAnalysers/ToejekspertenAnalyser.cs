b56e1bf8c64a740fe2802675d685cbf3c2e849c0
Rolle <rolle474@gmail.com>
2024-11-27 17:08:15 +0100

    Need to implement analyze function in ToejekspertenAnalyser.cs

--- Model/Utility/ProductAnalysers/ToejekspertenAnalyser.cs
+++ Model/Utility/ProductAnalysers/ToejekspertenAnalyser.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using HtmlAgilityPack;
 
 namespace SaleCheck.Model.Utility.ProductAnalysers;
 
@@ -35,14 +36,51 @@ public class ToejekspertenAnalyser : IProductAnalyser
         return sitemapPages; 
         
     }
-
-    public Task<List<Page>> Analyze(Page page)
+    public async Task<List<Page>> GetSitemapLinks(Page page)
     {
+        string html = await page.GetHtmlContent();
+
+        if (html == null) return new List<Page>();
+        
+        string pattern = @">https://[^\s<]+<";
+        MatchCollection matches = Regex.Matches(html, pattern);
+        List<Page> pages = new List<Page>();
+
+        foreach (Match match in matches)
+        {
+            string link = match.Value.Trim('>', '<');
+            pages.Add(new Page(SampleSites.VinduesGrossisten.Title,link));
+        }
+
+        return pages;
+        
+    }
+    
+    public async Task<List<ProductItem>> Analyze(Page page)
+    {
+        List<ProductItem> products = new List<ProductItem>();
+        var htmlDoc = new HtmlDocument();
+        string? content = await page.GetHtmlContent();
+        if (content == null) return new List<ProductItem>();
+        htmlDoc.LoadHtml(content);
+        
+        var productNodes = htmlDoc.DocumentNode.SelectNodes(
+            "//a[contains(@class, 'ela0j04e0') and contains(@class, 'css-12clykb') and contains(@class, 'e127r8ub0')]");
+
+        if (productNodes == null)
+        {
+            return products;
+        }
+
+        foreach (var productNode in productNodes)
+        {
+            
+        }
+        
+        
+        
         throw new NotImplementedException();
     }
 
-    public Task<List<Page>> GetSitemapLinks(Page page)
-    {
-        throw new NotImplementedException();
-    }
+    
 }
