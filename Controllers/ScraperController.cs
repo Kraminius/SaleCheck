69ee3afaf27492e17d3d46ab4cb2fd3ff8918f22
Mikkel Jürs <s224279@dtu.dk>
2024-11-27 17:04:26 +0100

    Endpoint for running the scraper manually. Needed after we got hacked.

---
+++ Controllers/ScraperController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaleCheck.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ScraperController : ControllerBase
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public ScraperController(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    [HttpPost("run")]
+    public async Task<IActionResult> RunScraper()
+    {
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var scheduledTaskService = scope.ServiceProvider.GetRequiredService<ScheduledTaskService>();
+            if (scheduledTaskService == null)
+            {
+                return StatusCode(500, "ScheduledTaskService could not be resolved.");
+            }
+
+            try
+            {
+                Console.WriteLine("Manually triggering the scraper.");
+                await scheduledTaskService.RunScheduledTask(CancellationToken.None);
+                return Ok("Scraper run successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error occurred while running the scraper: {e.Message}");
+                return StatusCode(500, $"Error: {e.Message}");
+            }
+        }
+    }
+}
